7155591759110751536559b467e086bffcbe7316
linux5396 <929159338@qq.com>
2018-07-31 17:04:55 +0800

    version 1.5

---
+++ src/main/java/com/qg/www/beans/Message.java
@@ -0,0 +1,42 @@
+package com.qg.www.beans;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author linxu
+ * @version 1.5
+ * 动态消息实体类；
+ */
+@Setter
+@Getter
+public class Message {
+    /**
+     * 消息的编号；
+     */
+    @SerializedName("messageid")
+    private int messageId;
+    /**
+     * 消息的发生时间；
+     */
+    @SerializedName("time")
+    private String time;
+    /**
+     * 消息的内容；
+     */
+    @SerializedName("content")
+    private String content;
+    /**
+     * 操作人ID
+     */
+    @SerializedName("operatorid")
+    private int operatorId;
+    /**
+     *用户ID；
+     */
+    @SerializedName("userid")
+    private int userId;
+
+}
+
